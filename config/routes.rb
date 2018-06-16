a4e15f1d63b8e9b8eddb5f0101c8266cb66545d9
Justine Clarke <justineclarke_2017@depauw.edu>
2018-06-16 19:30:38 +0000

    Fixed logouts

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/logout',  to: 'sessions#destroy'
   
   resources:users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
